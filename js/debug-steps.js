4c3bd8886529c14c71cc039ce2af2742c6df485c
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-09-19 02:55:51 +0600

    withdrow js start

--- js/debug-steps.js
+++ js/debug-steps.js
@@ -1,8 +1,8 @@
 /* 
 Debug Steps: 
 1. Error check (error reproduce).
-2. Check others stuffs on the website
-3. Check Console for error
+2. Check others stuffs on the website.
+3. Check Console for error.
 4. click on the link of the error (it will take you to the code)
 5. If needed add a breakpoint and try to stop the code over there and see the values
 6. if needed console log output
