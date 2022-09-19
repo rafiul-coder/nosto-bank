eae85fe42c635640030114d75935c81e4c45d561
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-09-20 00:19:47 +0600

    deposite finished

--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,7 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
   return inputFieldValue;
 }
 
